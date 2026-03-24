19150082aa1ea5ed0fddd722f6c63043ebd088a7
c2265232-dot <c2265232@gmail.com>
2026-03-24 07:23:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 let games = [
-  {"name":"Crazy Cattle 3D","url":"https://crazy-cattle.github.io/","img":"https://crazy-cattle.github.io/icon.png"},
-  {"name":"Slope","url":"https://3kh0.github.io/projects/slope/","img":"https://images.crazygames.com/games/slope/cover-160x160.png"},
-  {"name":"Run 3","url":"https://3kh0.github.io/projects/run-3/","img":"https://images.crazygames.com/games/run-3/cover-160x160.png"},
-  {"name":"Tunnel Rush","url":"https://3kh0.github.io/projects/tunnel-rush/","img":"https://images.crazygames.com/games/tunnel-rush/cover-160x160.png"},
-  {"name":"Drift Hunters","url":"https://3kh0.github.io/projects/drift-hunters/","img":"https://images.crazygames.com/games/drift-hunters/cover-160x160.png"},
-  {"name":"Retro Bowl","url":"https://3kh0.github.io/projects/retro-bowl/","img":"https://images.crazygames.com/games/retro-bowl/cover-160x160.png"},
-  {"name":"Moto X3M","url":"https://3kh0.github.io/projects/moto-x3m/","img":"https://images.crazygames.com/games/moto-x3m/cover-160x160.png"},
-  {"name":"Stickman Hook","url":"https://3kh0.github.io/projects/stickman-hook/","img":"https://images.crazygames.com/games/stickman-hook/cover-160x160.png"},
-  {"name":"Basketball Stars","url":"https://3kh0.github.io/projects/basketball-stars/","img":"https://images.crazygames.com/games/basketball-stars/cover-160x160.png"},
-  {"name":"Fireboy & Watergirl","url":"https://3kh0.github.io/projects/fireboy-watergirl/","img":"https://images.crazygames.com/games/fireboy-and-watergirl/cover-160x160.png"}
+{"name":"Crazy Cattle 3D","url":"https://crazy-cattle.github.io/","img":"https://crazy-cattle.github.io/icon.png"},
+{"name":"Slope","url":"https://3kh0.github.io/projects/slope/","img":"https://images.crazygames.com/games/slope/cover-160x160.png"},
+{"name":"Run 3","url":"https://3kh0.github.io/projects/run-3/","img":"https://images.crazygames.com/games/run-3/cover-160x160.png"},
+{"name":"Tunnel Rush","url":"https://3kh0.github.io/projects/tunnel-rush/","img":"https://images.crazygames.com/games/tunnel-rush/cover-160x160.png"},
+{"name":"Drift Hunters","url":"https://3kh0.github.io/projects/drift-hunters/","img":"https://images.crazygames.com/games/drift-hunters/cover-160x160.png"},
+{"name":"Retro Bowl","url":"https://3kh0.github.io/projects/retro-bowl/","img":"https://images.crazygames.com/games/retro-bowl/cover-160x160.png"},
+{"name":"Moto X3M","url":"https://3kh0.github.io/projects/moto-x3m/","img":"https://images.crazygames.com/games/moto-x3m/cover-160x160.png"},
+{"name":"Stickman Hook","url":"https://3kh0.github.io/projects/stickman-hook/","img":"https://images.crazygames.com/games/stickman-hook/cover-160x160.png"},
+{"name":"Basketball Stars","url":"https://3kh0.github.io/projects/basketball-stars/","img":"https://images.crazygames.com/games/basketball-stars/cover-160x160.png"},
+{"name":"Fireboy & Watergirl","url":"https://3kh0.github.io/projects/fireboy-watergirl/","img":"https://images.crazygames.com/games/fireboy-and-watergirl/cover-160x160.png"},
+
+{"name":"Vex 6","url":"https://3kh0.github.io/projects/vex-6/","img":"https://images.crazygames.com/games/vex-6/cover-160x160.png"},
+{"name":"Vex 7","url":"https://3kh0.github.io/projects/vex-7/","img":"https://images.crazygames.com/games/vex-7/cover-160x160.png"},
+{"name":"FNAF","url":"https://3kh0.github.io/projects/fnaf/","img":"https://upload.wikimedia.org/wikipedia/en/3/3a/Five_Nights_at_Freddy%27s_cover.png"},
+{"name":"Baldi's Basics","url":"https://3kh0.github.io/projects/baldis-basics/","img":"https://upload.wikimedia.org/wikipedia/en/2/2f/Baldis_Basics_cover.png"},
+{"name":"Minecraft Classic","url":"https://classic.minecraft.net/","img":"https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png"},
+
+{"name":"Agar.io","url":"https://agar.io/","img":"https://upload.wikimedia.org/wikipedia/en/1/19/Agar.io_logo.png"},
+{"name":"Slither.io","url":"https://slither.io/","img":"https://upload.wikimedia.org/wikipedia/en/3/32/Slither.io_logo.png"},
+{"name":"Diep.io","url":"https://diep.io/","img":"https://diep.io/favicon.ico"},
+{"name":"Zombs Royale","url":"https://zombsroyale.io/","img":"https://zombsroyale.io/img/icon.png"},
+{"name":"Krunker.io","url":"https://krunker.io/","img":"https://krunker.io/img/favicon.png"},
+
+{"name":"Shell Shockers","url":"https://shellshock.io/","img":"https://shellshock.io/img/logo.png"},
+{"name":"Paper.io 2","url":"https://paper-io.com/","img":"https://images.crazygames.com/games/paper-io-2/cover-160x160.png"},
+{"name":"Hole.io","url":"https://hole-io.com/","img":"https://images.crazygames.com/games/hole-io/cover-160x160.png"},
+{"name":"Flappy Bird","url":"https://flappybird.io/","img":"https://upload.wikimedia.org/wikipedia/en/0/0a/Flappy_Bird_icon.png"},
+{"name":"2048","url":"https://play2048.co/","img":"https://upload.wikimedia.org/wikipedia/commons/1/18/2048_logo.svg"},
+
+{"name":"Minesweeper","url":"https://minesweeperonline.com/","img":"https://upload.wikimedia.org/wikipedia/commons/5/5a/Minesweeper_Icon.png"},
+{"name":"Tetris","url":"https://tetris.com/play-tetris","img":"https://upload.wikimedia.org/wikipedia/commons/7/7c/Tetris_logo.png"},
+{"name":"Pacman","url":"https://www.google.com/logos/2010/pacman10-i.html","img":"https://upload.wikimedia.org/wikipedia/en/5/59/Pac-man.png"},
+{"name":"Doodle Jump","url":"https://poki.com/en/g/doodle-jump","img":"https://images.crazygames.com/games/doodle-jump/cover-160x160.png"},
+{"name":"Jetpack Joyride","url":"https://poki.com/en/g/jetpack-joyride","img":"https://images.crazygames.com/games/jetpack-joyride/cover-160x160.png"},
+
+{"name":"Temple Run 2","url":"https://poki.com/en/g/temple-run-2","img":"https://images.crazygames.com/games/temple-run-2/cover-160x160.png"},
+{"name":"Subway Surfers","url":"https://poki.com/en/g/subway-surfers","img":"https://images.crazygames.com/games/subway-surfers/cover-160x160.png"},
+{"name":"Crossy Road","url":"https://poki.com/en/g/crossy-road","img":"https://images.crazygames.com/games/crossy-road/cover-160x160.png"},
+{"name":"Drive Mad","url":"https://poki.com/en/g/drive-mad","img":"https://images.crazygames.com/games/drive-mad/cover-160x160.png"},
+{"name":"Blumgi Rocket","url":"https://poki.com/en/g/blumgi-rocket","img":"https://images.crazygames.com/games/blumgi-rocket/cover-160x160.png"},
+
+{"name":"Stick War","url":"https://poki.com/en/g/stickman-war","img":"https://images.crazygames.com/games/stickman-war/cover-160x160.png"},
+{"name":"Stick Fighter","url":"https://poki.com/en/g/stickman-fighter","img":"https://images.crazygames.com/games/stickman-fighter/cover-160x160.png"},
+{"name":"Basket Random","url":"https://poki.com/en/g/basket-random","img":"https://images.crazygames.com/games/basket-random/cover-160x160.png"},
+{"name":"Soccer Random","url":"https://poki.com/en/g/soccer-random","img":"https://images.crazygames.com/games/soccer-random/cover-160x160.png"},
+{"name":"Volley Random","url":"https://poki.com/en/g/volley-random","img":"https://images.crazygames.com/games/volley-random/cover-160x160.png"}
 ];
 
 /* ⭐ RATINGS */
