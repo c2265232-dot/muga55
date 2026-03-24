48f68eddf0f767b08bb15b6d764f902aa20624f0
c2265232-dot <c2265232@gmail.com>
2026-03-24 07:20:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ let games = [
   {"name":"Run 3","url":"https://3kh0.github.io/projects/run-3/","img":"https://images.crazygames.com/games/run-3/cover-160x160.png"},
   {"name":"Tunnel Rush","url":"https://3kh0.github.io/projects/tunnel-rush/","img":"https://images.crazygames.com/games/tunnel-rush/cover-160x160.png"},
   {"name":"Drift Hunters","url":"https://3kh0.github.io/projects/drift-hunters/","img":"https://images.crazygames.com/games/drift-hunters/cover-160x160.png"},
-  {"name":"Retro Bowl","url":"https://3kh0.github.io/projects/retro-bowl/","img":"https://images.crazygames.com/games/retro-bowl/cover-160x160.png"}
+  {"name":"Retro Bowl","url":"https://3kh0.github.io/projects/retro-bowl/","img":"https://images.crazygames.com/games/retro-bowl/cover-160x160.png"},
+  {"name":"Moto X3M","url":"https://3kh0.github.io/projects/moto-x3m/","img":"https://images.crazygames.com/games/moto-x3m/cover-160x160.png"},
+  {"name":"Stickman Hook","url":"https://3kh0.github.io/projects/stickman-hook/","img":"https://images.crazygames.com/games/stickman-hook/cover-160x160.png"},
+  {"name":"Basketball Stars","url":"https://3kh0.github.io/projects/basketball-stars/","img":"https://images.crazygames.com/games/basketball-stars/cover-160x160.png"},
+  {"name":"Fireboy & Watergirl","url":"https://3kh0.github.io/projects/fireboy-watergirl/","img":"https://images.crazygames.com/games/fireboy-and-watergirl/cover-160x160.png"}
 ];
 
-/* =====================
-   RATINGS SYSTEM
-===================== */
+/* ⭐ RATINGS */
 let ratings = JSON.parse(localStorage.getItem("ratings") || "{}");
 
 function rateGame(url, value){
   ratings[url] = value;
   localStorage.setItem("ratings", JSON.stringify(ratings));
-  loadGames(games);
+  loadGames();
 }
 
 function getRating(url){
@@ -33,10 +35,8 @@ function renderStars(url){
   return stars;
 }
 
-/* =====================
-   LOAD GAMES
-===================== */
-function loadGames(list){
+/* LOAD GAMES */
+function loadGames(list = games){
   document.getElementById("games").innerHTML =
     list.map(g=>`
       <div class="card" onclick="openGame('${g.url}')">
@@ -47,16 +47,12 @@ function loadGames(list){
     `).join("");
 }
 
-/* =====================
-   OPEN GAME
-===================== */
+/* OPEN GAME */
 function openGame(url){
   location.href = "game.html?url=" + encodeURIComponent(url);
 }
 
-/* =====================
-   SEARCH
-===================== */
+/* SEARCH */
 document.getElementById("search").oninput = e=>{
   let v = e.target.value.toLowerCase();
   loadGames(games.filter(g=>g.name.toLowerCase().includes(v)));
